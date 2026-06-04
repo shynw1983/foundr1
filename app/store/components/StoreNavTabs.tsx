d2bba3ebfed6b5d482dbe72f22ebd0c5aaaa89e7
maruKyu <admin@maru-kyu.jp>
2026-06-04 16:12:41 +0900

    Improve store mobile header spacing

--- app/store/components/StoreNavTabs.tsx
+++ app/store/components/StoreNavTabs.tsx
@@ -42,7 +42,6 @@ function formatStoreClock(date: Date) {
     timeZone: "Asia/Tokyo",
     hour: "2-digit",
     minute: "2-digit",
-    second: "2-digit",
     hour12: false
   }).format(date);
   return { dateText, timeText };
